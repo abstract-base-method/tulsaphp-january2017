304f2afb83ab2db4c2ecac1eb7ad7e05551b2bdf
James E. Bell Jr <github@travelerbell.net>
2017-01-05 21:10:15 -0600

    Added testing capability of 3
    Signed-off-by: James E. Bell Jr <github@travelerbell.net>
    
    	modified:   3/public/mydb.sqlite
    	modified:   3/src/Demo/Procedures/Register.php
    		Added switch to 'test' via using in memory SQLite DB
    	modified:   readme.MD
    		I decided not to do a fourth demo instead just make testing a part of 3

--- 3/src/Demo/Procedures/Register.php
+++ 3/src/Demo/Procedures/Register.php
@@ -11,21 +11,46 @@ namespace Demo\Procedures;
 class Register implements \Demo\iFace\Register
 {
 
-    public function AttemptRegistration(string $Email): bool
+    public function AttemptRegistration(string $Email, bool $Test = false): bool
     {
-        $PDO = new \PDO("sqlite:mydb.sqlite");
-        $Query = "
+        if ($Test) {
+            $PDO = new \PDO("sqlite::memory:");
+            $Query = "
+                CREATE TABLE just_demo
+                (
+                    id INTEGER PRIMARY KEY NOT NULL,
+                    address TEXT NOT NULL
+                )
+            ";
+            $PDO->exec($Query);
+            $Query = "
+            INSERT INTO
+              just_demo
+            (address)
+            VALUES
+            (:address)
+            ";
+            $Query = $PDO->prepare($Query);
+            if ($Query->execute(array(':address' => $Email))) {
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            $PDO = new \PDO("sqlite:mydb.sqlite");
+            $Query = "
             INSERT INTO
               just_demo
             (address) 
             VALUES 
             (:address)
         ";
-        $Query = $PDO->prepare($Query);
-        if ($Query->execute(array(':address' => $Email))) {
-            return true;
-        } else {
-            return false;
+            $Query = $PDO->prepare($Query);
+            if ($Query->execute(array(':address' => $Email))) {
+                return true;
+            } else {
+                return false;
+            }
         }
     }
 }
